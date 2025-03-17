f132137eaeae82b0a983a71f6844902c3178449b
fpvenchi <spam@filippulkert.site>
2025-03-17 18:06:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,47 @@ const port = 3000;
 
 app.use(express.json());
 
-app.post('/save', (req, res) => {
+// Registrace uživatele
+app.post('/register', (req, res) => {
     const { username, password } = req.body;
 
-    const data = `${username}:${password}\n`;
+    const users = loadUsers();
+    if (users[username]) {
+        return res.status(400).send('User already exists');
+    }
 
-    fs.appendFileSync('users.txt', data);
+    users[username] = password;
+    saveUsers(users);
+
     res.sendStatus(200);
 });
 
+// Přihlášení uživatele
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    const users = loadUsers();
+    if (users[username] && users[username] === password) {
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(401);
+    }
+});
+
+// Načtení uživatelů ze souboru
+function loadUsers() {
+    if (!fs.existsSync('users.txt')) {
+        return {};
+    }
+    const data = fs.readFileSync('users.txt', 'utf-8');
+    return JSON.parse(data || '{}');
+}
+
+// Uložení uživatelů do souboru
+function saveUsers(users) {
+    fs.writeFileSync('users.txt', JSON.stringify(users));
+}
+
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server běží na http://localhost:${port}`);
 });
